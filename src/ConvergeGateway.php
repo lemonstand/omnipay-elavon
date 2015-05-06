58808f758d96bb802eb555210172c6663824f828
Andy Perdomo <andres.a.perdomo@gmail.com>
2015-05-06 15:25:34 -0700

    Adding setters/getters for default parameters.

--- src/ConvergeGateway.php
+++ src/ConvergeGateway.php
@@ -22,4 +22,34 @@ class ConvergeGateway extends AbstractGateway
             'password' => '',
         );
     }
+
+    public function getMerchantId()
+    {
+        return $this->getParameter('merchantId');
+    }
+
+    public function setMerchantId($value)
+    {
+        return $this->setParameter('merchantId', $value);
+    }
+
+    public function getUsername()
+    {
+        return $this->getParameter('username');
+    }
+
+    public function setUsername($value)
+    {
+        return $this->setParameter('username', $value);
+    }
+
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    public function setPassword($value)
+    {
+        return $this->setParameter('password', $value);
+    }
 }
